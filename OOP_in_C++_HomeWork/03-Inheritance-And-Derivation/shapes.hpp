1f5e710aa6ee2e27fb860f29f31592737b559a70
name1e5s <836401406@qq.com>
2019-03-03 13:52:07 +0800

    C

---
+++ OOP_in_C++_HomeWork/03-Inheritance-And-Derivation/shapes.hpp
@@ -0,0 +1,60 @@
+#ifndef SHAPES
+#define SHAPES
+#include <cmath>
+#include <exception>
+#include <iostream>
+#include <stdexcept>
+
+using std::cout;
+using std::endl;
+
+struct NotImplemented : public std::logic_error {
+  NotImplemented() : std::logic_error("Function Not Implemented Yet!") {}
+};
+
+class Shape {
+public:
+  Shape() { cout << "Shape::Shape() constructor called." << endl; }
+  ~Shape() { cout << "Shape::~Shape() deconstructor called." << endl; }
+  virtual double area(void) { throw NotImplemented(); }
+};
+
+class Rectangle : public Shape {
+public:
+  Rectangle(double w = 0, double h = 0) : Shape(), width(w), height(h) {
+    cout << "Rectangle::Rectangle() constructor called." << endl;
+  }
+  ~Rectangle() {
+    cout << "Rectangle::~Rectangle() deconstructor called." << endl;
+  }
+  virtual double area(void) { return width * height; }
+
+protected:
+  double width, height;
+};
+
+class Square : public Rectangle {
+public:
+  Square(double l = 0) : Rectangle(l, l) {
+    cout << "Square::Square() constructor called." << endl;
+  }
+  ~Square() { cout << "Square::~Square() deconstructor called." << endl; }
+};
+
+class Circle : public Shape {
+public:
+  Circle(double r = 0) : Shape(), radius(r) {
+    cout << "Circle::Circle() constructor called" << endl;
+  }
+
+  ~Circle() { cout << "Circle::~Circle() deconstrctor called." << endl; }
+  double area(void) {
+#define _USE_MATH_DEFINES
+    return M_PI * radius * radius;
+#undef _USE_MATH_DEFINES
+  }
+
+private:
+  double radius;
+};
+#endif
