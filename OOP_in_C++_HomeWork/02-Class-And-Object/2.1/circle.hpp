4a73608a6e846fa45cdbd0ccec7c192c2a22f235
name1e5s <name1e5s@qq.com>
2019-03-08 23:21:59 +0800

    fix typo

--- OOP_in_C++_HomeWork/02-Class-And-Object/2.1/circle.hpp
+++ OOP_in_C++_HomeWork/02-Class-And-Object/2.1/circle.hpp
@@ -6,9 +6,9 @@ class Point {
 public:
   double x, y;
   Point(double x = 0, double y = 0) : x(x), y(y) {
-    std::cout << "Circle::Circle() called." << std::endl;
+    std::cout << "Point::Point called." << std::endl;
   }
-  ~Point() { std::cout << "Circle destroied." << std::endl; }
+  ~Point() { std::cout << "Point destroyed." << std::endl; }
   double distance(const Point &p) {
     return std::sqrt((x - p.x) * (x - p.x) + (y - p.y) * (y - p.y));
   }
@@ -25,9 +25,9 @@ public:
     std::cin >> p.x >> p.y >> r;
   };
   circle(double x = 0, double y = 0, double r = 0) : p(x, y), r(r) {
-    std::cout << "constructed." << std::endl;
+    std::cout << "Circle::Circle called.." << std::endl;
   }
-  ~circle() { std::cout << "deconstructed." << std::endl; }
+  ~circle() { std::cout << "Circle destroyed." << std::endl; }
 
   bool isCross(const circle &maru) {
     double d = p.distance(maru.p);
