e0f62f7426980edb45757b5f8ef54dab25b1cd35
name1e5s <name1e5s@qq.com>
2021-11-21 09:56:10 +0800

    update kwic

---
+++ kwic/README.md
@@ -0,0 +1,5 @@
+# KWIC 索引系统
+
+该系统为软件体系结构2021第一次作业的实现。
+
+在线报告链接：https://khbgo05wz2.feishu.cn/docs/doccn1PxL6MOE90Yvpt5AsHjuvc
--- kwic/src/com/name1e5s/kwic/Main.java
+++ kwic/src/com/name1e5s/kwic/Main.java
@@ -6,28 +6,51 @@ import java.net.Socket;
 import java.util.Arrays;
 
 public class Main {
-
     public static void main(String[] args) {
-        if (Arrays.stream(args).anyMatch(s -> s.contains("tcp"))) {
+        if (Arrays.stream(args).anyMatch(s -> s.startsWith("tcp"))) {
             System.out.println("Running on TCP mode, listening on port 1145");
             try {
                 ServerSocket serverSocket = new ServerSocket(1145);
                 while (true) {
                     Socket socket = serverSocket.accept();
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(new DataInputStream(socket.getInputStream())));
-                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new DataOutputStream(socket.getOutputStream())));
-                    run(reader, writer);
-                    socket.close();
+                    new Thread(() -> {
+                        try {
+                            BufferedReader reader = new BufferedReader(new InputStreamReader(new DataInputStream(socket.getInputStream())));
+                            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new DataOutputStream(socket.getOutputStream())));
+                            run(reader, writer);
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }).start();
                 }
             } catch (Exception e) {
                 e.printStackTrace();
                 System.exit(1);
             }
         } else {
-            System.out.println("Running on STDIO mode");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+            BufferedReader reader = null;
+            BufferedWriter writer = null;
+
+            if (args.length == 2) {
+                System.out.println("Running on FILE IO mode");
+                System.out.printf("Input file path: %s\n", args[0]);
+                System.out.printf("Output file path: %s\n", args[1]);
+
+                try {
+                    reader = new BufferedReader(new InputStreamReader(new FileInputStream(args[0])));
+                    writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(args[1])));
+                } catch (Exception e) {
+                    System.out.println("Open file failed!");
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("Running on STDIO mode");
+                reader = new BufferedReader(new InputStreamReader(System.in));
+                writer = new BufferedWriter(new OutputStreamWriter(System.out));
+            }
             run(reader, writer);
+            System.out.println("Done.");
         }
     }
 
